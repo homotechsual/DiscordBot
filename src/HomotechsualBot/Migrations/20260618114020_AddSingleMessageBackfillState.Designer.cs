d8ec4b9789c8c12308036ad2b416cf87051ea319
Mikey O'Toole <mikey@homotechsual.dev>
2026-06-18 12:48:05 +0100

    fix(migrations): add singlemessage backfill migration

--- src/HomotechsualBot/Migrations/20260618114020_AddSingleMessageBackfillState.Designer.cs
+++ src/HomotechsualBot/Migrations/20260618114020_AddSingleMessageBackfillState.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DiscordBot.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomotechsualBot.Migrations
 {
     [DbContext(typeof(HomotechsualBotContext))]
-    partial class HomotechsualBotContextModelSnapshot : ModelSnapshot
+    [Migration("20260618114020_AddSingleMessageBackfillState")]
+    partial class AddSingleMessageBackfillState
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.9");
---
+++ src/HomotechsualBot/Migrations/20260618114020_AddSingleMessageBackfillState.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomotechsualBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSingleMessageBackfillState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "BackfillBeforeMessageId",
+                table: "SingleMessageChannelStates",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "BackfillInProgress",
+                table: "SingleMessageChannelStates",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<ulong>(
+                name: "GuildId",
+                table: "SingleMessageChannelStates",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BackfillBeforeMessageId",
+                table: "SingleMessageChannelStates");
+
+            migrationBuilder.DropColumn(
+                name: "BackfillInProgress",
+                table: "SingleMessageChannelStates");
+
+            migrationBuilder.DropColumn(
+                name: "GuildId",
+                table: "SingleMessageChannelStates");
+        }
+    }
+}
